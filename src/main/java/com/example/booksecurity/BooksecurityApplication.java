1cd7f798af62e586d64b360de009864bd51fd629
anxgh-n <anaghmanoj@gmail.com>
2024-10-22 15:17:43 +0530

    security

---
+++ src/main/java/com/example/booksecurity/BooksecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksecurityApplication.class, args);
+	}
+
+}
